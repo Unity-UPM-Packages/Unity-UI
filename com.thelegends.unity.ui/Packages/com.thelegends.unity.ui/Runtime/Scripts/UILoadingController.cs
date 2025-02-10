f28a275e38c4b92d2c83d1d16353937248bb4fd0
QuangHaiVu <zzquanghai0108zz@gmail.com>
2025-02-10 14:29:01 +0700

    fix: UILoading 1.0.0

---
+++ com.thelegends.unity.ui/Packages/com.thelegends.unity.ui/Runtime/Scripts/UILoadingController.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+namespace TheLegends.Base.UI
+{
+    public class UILoadingController : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject container;
+
+        [SerializeField]
+        private Image backgroundImg;
+
+        [SerializeField]
+        private Sprite[] backgroundSprites;
+
+        [SerializeField]
+        private Image fillImg;
+
+        [SerializeField]
+        private string statusString = "Loading... please wait!";
+        [SerializeField]
+        private Text statusTxt;
+
+        private int spriteIndex = 0;
+        private float eslapseTime = 0;
+        private int statusSuffixIndex = 0;
+        private string statusSuffixString = ".";
+
+        protected static UILoadingController instance = null;
+
+        private void Awake()
+        {
+            instance = this;
+            instance.container.SetActive(false);
+            instance.fillImg.fillAmount = 0;
+        }
+
+        public static void Show(float time, Action onComplete = null)
+        {
+            instance.spriteIndex = instance.spriteIndex % instance.backgroundSprites.Length;
+            instance.backgroundImg.sprite = instance.backgroundSprites[instance.spriteIndex];
+            instance.spriteIndex++;
+
+            instance.container.SetActive(true);
+            FillLoadingProgressBar(1, time, () =>
+            {
+                onComplete?.Invoke();
+                Hide();
+            });
+        }
+
+        public static void SetProgress(float percent, Action onComplete = null)
+        {
+            instance.container.SetActive(true);
+            FillLoadingProgressBar(percent, percent, onComplete);
+        }
+
+        private static void FillLoadingProgressBar(float value, float time, Action onComplete = null)
+        {
+            instance.fillImg.DOKill();
+            instance.fillImg.DOFillAmount(value, time).OnComplete(() =>
+            {
+                onComplete?.Invoke();
+            }).OnStart(() =>
+            {
+                instance.eslapseTime = 0;
+            }).OnUpdate(() =>
+            {
+                instance.eslapseTime += Time.deltaTime;
+                instance.statusSuffixIndex = Mathf.Clamp((int)instance.eslapseTime % 3, 0, 2);
+
+                switch (instance.statusSuffixIndex)
+                {
+                    case 0:
+                        instance.statusSuffixString = ".";
+                        break;
+                    case 1:
+                        instance.statusSuffixString = "..";
+                        break;
+                    case 2:
+                        instance.statusSuffixString = "...";
+                        break;
+                }
+
+                instance.statusTxt.text = instance.statusString + " " + instance.statusSuffixString;
+            });
+        }
+
+        private static void Hide()
+        {
+            instance.fillImg.DOKill();
+            instance.container.SetActive(false);
+            instance.fillImg.fillAmount = 0;
+        }
+    }
+}
