002008d5bc4a4793a89f1997c1795e0883afe9d2
QuangHaiVu <zzquanghai0108zz@gmail.com>
2025-08-08 05:24:43 +0700

    aaa

--- com.thelegends.unity.ui/Assets/Example/ShowVase/ShowCaseAnimPlay.cs
+++ com.thelegends.unity.ui/Assets/Example/ShowVase/ShowCaseAnimPlay.cs
@@ -3,26 +3,16 @@ using System.Collections.Generic;
 using TheLegends.Base.UI;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class ShowCaseAnimPlay : MonoBehaviour
 {
     public UIAnimationGroup[] uIAnimationGroups;
 
-    void Start()
-    {
-        A();
-    }
-
-    public void A()
+    public void PlayAll()
     {
         foreach (var group in uIAnimationGroups)
         {
             group.Play();
         }
-    }
-
-    public void B(float value)
-    {
-        Debug.Log("value: " + value);
     }
     
 }
